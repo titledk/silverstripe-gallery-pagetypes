693c6ddb0dfb633d5e42162d3a06ef36182bc3db
Anselm Christophersen <ac@anselm.dk>
2016-03-24 23:38:19 +0100

    upload dir rules upgrade

--- code/GalleryPage.php
+++ code/GalleryPage.php
@@ -7,27 +7,46 @@
  * @date   March 2016
  *
  * StartGeneratedWithDataObjectAnnotator
- * @property int AssetsFolderID
- * @method Folder AssetsFolder
  * @method ManyManyList|Image[] Images
- * @mixin AssetsFolderExtension
- * @mixin UploadDirRules_SiteTreeExtension
  * @mixin GalleryExtension
  * EndGeneratedWithDataObjectAnnotator
  */
-class GalleryPage extends Page
+class GalleryPage extends Page implements UploadDirRulesInterface
 {
     private static $singular_name = 'Gallery';
     private static $plural_name = 'Galleries';
     private static $description = 'A page type for listing images';
     private static $allowed_children = [];
     private static $icon = 'gallery-pagetypes/images/pageicons/gallery.png';
-
     private static $extensions = [
-        'AssetsFolderExtension',
-        'UploadDirRules_SiteTreeExtension', //TODO this should go away
         'GalleryExtension'
     ];
+
+    /**
+     * The gallery directory is created based on the page name
+     * You can overwrite this behavior by extending this page
+     * TODO this could also be made configurable
+     * @return string
+     */
+    function getCalcAssetsFolderDirectory() {
+        if ($this->ID) {
+            $filter = URLSegmentFilter::create();
+            return Config::inst()->get('GalleryExtension', 'gallery_folder') .
+                '/' . $this->ID . '-' . $filter->filter($this->Title);
+        }
+    }
+    function getMessageSaveFirst(){
+        return 'Please pick a name and save to create corresponding directory';
+    }
+    function getMessageUploadDirectory() {
+        return null;
+    }
+    // Make sure that the directory is NOT saved before a page name has been chosen
+    function getReadyForFolderCreation() {
+        if ($this->Title != 'New ' . self::$singular_name) {
+            return true;
+        }
+    }
 }
 
 class GalleryPage_Controller extends Page_Controller
